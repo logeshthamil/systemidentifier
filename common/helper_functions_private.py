7ee89247efe1f4abc5f2df3f23dcc05f6c2e7750
Logesh <logeshthamil@gmail.com>
2016-08-12 10:54:37 +0200

    helper functions private modified

--- common/helper_functions_private.py
+++ common/helper_functions_private.py
@@ -3,12 +3,11 @@ import math
 import sumpf
 
 
-def cut_spectrum(inputspectrum, freq_range):
-    # TODO: why is this function called "cut...", when it actually appends something
+def cut_spectrum(input_spectrum, desired_frequency_range):
     """
-    Appends zero outside the desired frequency range of the spectrum.
-    :param inputspectrum: the input spectrum to which zero has to be appended outside the desired frequency range
-    :param freq_range: the desired freqency range
+    Cut the input spectrum to the desired frequency range. It appends zero outside the desired frequency range.
+    :param input_spectrum: the input spectrum to which zero has to be appended outside the desired frequency range
+    :param desired_frequency_range: the desired freqency range
     :return: the modified spectrum
     """
     channels_ip = []
@@ -16,32 +15,32 @@ def cut_spectrum(inputspectrum, freq_range):
     # Python hints:
     #    [0.0] * 3 = [0.0, 0.0, 0.0]
     #    a = [1.0]; a.extend([2.0, 3.0]) => a = [1.0, 2.0, 3.0]
-    for ip in inputspectrum.GetChannels():
+    for ip in input_spectrum.GetChannels():
         channel_ip = []
         channel_op = []
         for n, i in enumerate(ip):
-            if n > freq_range[0] / inputspectrum.GetResolution() and n < freq_range[1] / inputspectrum.GetResolution():
+            if n > desired_frequency_range[0] / input_spectrum.GetResolution() and n < desired_frequency_range[1] / \
+                    input_spectrum.GetResolution():
                 channel_ip.append(i)
             else:
                 channel_ip.append(0.0)
                 channel_op.append(0.0)
         channels_ip.append(tuple(channel_ip))
-    input_spectrum = sumpf.Spectrum(channels=tuple(channels_ip), resolution=inputspectrum.GetResolution(),
-                                    labels=inputspectrum.GetLabels())
-    return input_spectrum
+    input_spectrum_modified = sumpf.Spectrum(channels=tuple(channels_ip), resolution=input_spectrum.GetResolution(),
+                                    labels=input_spectrum.GetLabels())
+    return input_spectrum_modified
 
 
-def calculateenergy_timedomain(input):
-    # TODO: input is a builtin function of Python. Don't use this as a variable name
+def calculateenergy_timedomain(input_signal_or_spectrum):
     """
     Calculates the energy of the input in time domain.
     :param input: the input signal or spectrum whose energy has to be calculated
     :return: the tuple of the energy of the input signal of different channels
     """
-    if isinstance(input, (sumpf.Spectrum)):
-        ip = sumpf.modules.InverseFourierTransform(spectrum=input).GetSignal()
+    if isinstance(input_signal_or_spectrum, (sumpf.Spectrum)):
+        ip = sumpf.modules.InverseFourierTransform(spectrum=input_signal_or_spectrum).GetSignal()
     else:
-        ip = input
+        ip = input_signal_or_spectrum
     energy_allchannels = []
     for c in ip.GetChannels():
         energy_singlechannel = []
@@ -51,17 +50,16 @@ def calculateenergy_timedomain(input):
     return energy_allchannels
 
 
-def calculateenergy_freqdomain(input):
-    # TODO: input is a builtin function of Python. Don't use this as a variable name
+def calculateenergy_freqdomain(input_signal_or_spectrum):
     """
     Calculates the energy of the input in frequency domain
     :param input: the input signal or spectrum whose energy has to be calculated
     :return: the tuple of the energy of the input spectrum of different channels
     """
-    if isinstance(input, (sumpf.Signal)):
-        ip = sumpf.modules.FourierTransform(signal=input).GetSpectrum()
+    if isinstance(input_signal_or_spectrum, (sumpf.Signal)):
+        ip = sumpf.modules.FourierTransform(signal=input_signal_or_spectrum).GetSpectrum()
     else:
-        ip = input
+        ip = input_signal_or_spectrum
     energy_allchannels = []
     for c in ip.GetChannels():
         energy_singlechannel = []
@@ -71,19 +69,18 @@ def calculateenergy_freqdomain(input):
     return energy_allchannels
 
 
-def calculateenergy_betweenfreq_freqdomain(input, frequency_range):
-    # TODO: input is a builtin function of Python. Don't use this as a variable name
+def calculateenergy_betweenfreq_freqdomain(input_signal_or_spectrum, desired_frequency_range):
     """
     Calculates the energy of input signal between certain frequencies of input signal
     :param input: the input signal or spectrum whose energy has to be calculated
     :param frequency_range: the range of frequencies over which the energy has to be calculated
     :return: the tuple of the energy of input spectrum in frequency domain
     """
-    if isinstance(input, (sumpf.Signal)):
-        ip = sumpf.modules.FourierTransform(signal=input).GetSpectrum()
+    if isinstance(input_signal_or_spectrum, (sumpf.Signal)):
+        ip = sumpf.modules.FourierTransform(signal=input_signal_or_spectrum).GetSpectrum()
     else:
-        ip = input
-    spec = cut_spectrum(ip, frequency_range)
+        ip = input_signal_or_spectrum
+    spec = cut_spectrum(ip, desired_frequency_range)
     energy = calculateenergy_freqdomain(spec)
     return energy
 
