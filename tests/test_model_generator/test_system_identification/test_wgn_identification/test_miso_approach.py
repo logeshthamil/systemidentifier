2c8901ac3805d61adfce1067b05f58f4118d5d50
Logesh <logeshthamil@gmail.com>
2016-08-15 13:12:52 +0200

    miso identification working fine, need to modify system identification algorithms based on desired branches

--- tests/test_model_generator/test_system_identification/test_wgn_identification/test_miso_approach.py
+++ tests/test_model_generator/test_system_identification/test_wgn_identification/test_miso_approach.py
@@ -1,24 +1,28 @@
 import sumpf
 import nlsp
 
+
 def test_identify_an_HGM_MISO():
-    branches = 3
+    branches = 2
+    excitation_length = 2**14
+    sampling_rate = 48000
     aliasing_compensation = nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation()
     linear_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches, sampling_rate=48000.0)
-    nonlinear_functions = [nlsp.nonlinear_function.Power(i + 1) for i in range(branches)]
+    nonlinear_functions = [nlsp.nonlinear_function.Power(degree=i+1) for i in range(branches)]
     black_box = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
                                            aliasing_compensation=aliasing_compensation,
                                            filter_impulseresponses=linear_filters)
-    identification_algorithm = nlsp.system_identification.MISOapproach(select_branches=range(1, branches + 1), )
+    identification_algorithm = nlsp.system_identification.MISOapproach(select_branches=range(1, branches + 1), excitation_length=excitation_length,
+                                                                          excitation_sampling_rate=sampling_rate)
     excitation = identification_algorithm.GetExcitation()
     black_box.SetInput(excitation)
     identification_algorithm.SetResponse(response=black_box.GetOutput())
-    filt = identification_algorithm._GetFilterImpuleResponses()[0]
-    hm = nlsp.HammersteinModel(nonlinear_function=identification_algorithm._GetNonlinerFunctions()[0],
-                               filter_impulseresponse=filt)
-    hm.SetInput(excitation)
-    nlsp.plots.plot(hm.GetOutput())
-    # nlsp.plots.plot(sumpf.modules.FourierTransform(black_box.GetOutput()).GetSpectrum(),show=False)
-    # nlsp.plots.plot(model_black_box.GetOutput(),show=True)
-    # evaluation = nlsp.evaluations.CompareWithReference(black_box.GetOutput(), model_black_box.GetOutput())
-    # assert evaluation.GetSignaltoErrorRatio()[0] >= 40
+
+    model_black_box = identification_algorithm.GetOutputModel()
+
+    exc = sumpf.modules.NoiseGenerator(distribution=sumpf.modules.NoiseGenerator.UniformDistribution(),
+                                       samplingrate=48000, length=2 ** 16)
+    model_black_box.SetInput(exc.GetSignal())
+    black_box.SetInput(exc.GetSignal())
+    evaluation = nlsp.evaluations.CompareWithReference(black_box.GetOutput(), model_black_box.GetOutput())
+    assert evaluation.GetSignaltoErrorRatio()[0] >= 40
