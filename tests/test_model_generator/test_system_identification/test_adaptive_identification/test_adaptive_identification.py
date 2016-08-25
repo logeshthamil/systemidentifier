b240fa348ecc47a743e5cb63f3c6ad559b09cfd4
Logesh <logeshthamil@gmail.com>
2016-08-25 14:20:40 +0200

    accuracy of adaptive identification < accuracy of clipping adaptive identification

--- tests/test_model_generator/test_system_identification/test_adaptive_identification/test_adaptive_identification.py
+++ tests/test_model_generator/test_system_identification/test_adaptive_identification/test_adaptive_identification.py
@@ -81,3 +81,46 @@ def test_identify_a_HGM_clippingadaptive():
     black_box.SetInput(exc.GetSignal())
     evaluation = nlsp.evaluations.CompareWithReference(black_box.GetOutput(), model_black_box.GetOutput())
     assert evaluation.GetSignaltoErrorRatio() >= 75
+
+def test_identify_a_HGM_clippingvsclippingadaptive():
+    """
+    Test the comparison of accuracy between adaptive and clipping adaptive system identification.
+    """
+    branches = 2
+    excitation_length = 2**14
+    sampling_rate = 48000
+    select_branches = range(1, branches + 1)
+    aliasing_compensation = nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation()
+    linear_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches, sampling_rate=sampling_rate)
+    nonlinear_functions = [nlsp.nonlinear_function.Power(i + 1) for i in range(branches)]
+    black_box = nlsp.HammersteinGroupModel(nonlinear_functions=nonlinear_functions,
+                                           aliasing_compensation=aliasing_compensation,
+                                           filter_impulseresponses=linear_filters)
+    identification_algorithm_clippingadaptive = nlsp.system_identification.ClippingAdaptive(select_branches=select_branches,
+                                                                           excitation_length=excitation_length,
+                                                                           nonlinear_function=nlsp.nonlinear_function.HardClip,
+                                                                           thresholds=[[-1.0,1.0],[-0.8,0.9]])
+    identification_algorithm_adaptive = nlsp.system_identification.Adaptive(select_branches=select_branches,
+                                                                           excitation_length=excitation_length,
+                                                                           nonlinear_function=nlsp.nonlinear_function.Legendre)
+
+    excitation_adaptive = identification_algorithm_adaptive.GetExcitation()
+    black_box.SetInput(input_signal=excitation_adaptive)
+    identification_algorithm_adaptive.SetResponse(response=black_box.GetOutput())
+    model_black_box_adaptive = identification_algorithm_adaptive.GetOutputModel()
+
+    excitation_clippingadaptive = identification_algorithm_clippingadaptive.GetExcitation()
+    black_box.SetInput(input_signal=excitation_clippingadaptive)
+    identification_algorithm_clippingadaptive.SetResponse(response=black_box.GetOutput())
+    model_black_box_clippingadaptive = identification_algorithm_clippingadaptive.GetOutputModel()
+
+    exc = sumpf.modules.NoiseGenerator(distribution=sumpf.modules.NoiseGenerator.UniformDistribution(),
+                                       samplingrate=48000, length=2 ** 14, seed="signal")
+    model_black_box_adaptive.SetInput(exc.GetSignal())
+    model_black_box_clippingadaptive.SetInput(exc.GetSignal())
+    black_box.SetInput(exc.GetSignal())
+
+    evaluation_adaptive = nlsp.evaluations.CompareWithReference(black_box.GetOutput(), model_black_box_adaptive.GetOutput())
+    evaluation_clippingadaptive = nlsp.evaluations.CompareWithReference(black_box.GetOutput(), model_black_box_clippingadaptive.GetOutput())
+
+    assert evaluation_clippingadaptive.GetSignaltoErrorRatio()[0] > evaluation_adaptive.GetSignaltoErrorRatio()[0]
