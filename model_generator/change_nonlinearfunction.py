b4f61a82577d03a4c5670e6ef6b1a43ed15ae8e2
Logesh <logeshthamil@gmail.com>
2016-08-30 09:51:08 +0200

    recompute filter kernels works fine, HGM model generator has to be debugged

--- model_generator/change_nonlinearfunction.py
+++ model_generator/change_nonlinearfunction.py
@@ -30,10 +30,10 @@ class RecomputeFilterKernels(HGMModelGenerator):
         ref_nl = modify_model._nonlinear_functions
         ref_nl_specific = ref_nl[0].__class__
         ref_filter = modify_model._filter_impulseresponses
-        degree = []
+        degree_nl = []
         self._nonlinear_functions = []
         for nl in ref_nl:
-            degree.append(nl.GetMaximumHarmonics())
+            degree_nl.append(nl.GetMaximumHarmonics())
         if self.__nonlinear_function is None:
             for nl in ref_nl:
                 self._nonlinear_functions.append(nl.CreateModified())
@@ -43,38 +43,38 @@ class RecomputeFilterKernels(HGMModelGenerator):
                 self._nonlinear_functions.append(nl.CreateModified())
             self._filter_impulseresponses = ref_filter
         elif self.__nonlinear_function is nlsp.nonlinear_function.Power:
-            for i in degree:
+            for i in degree_nl:
                 self._nonlinear_functions.append(self.__nonlinear_function(degree=i))
             if self.__nonlinear_function is nlsp.nonlinear_function.Chebyshev:
                 polynomial = numpy.polynomial.chebyshev.Chebyshev
                 coefficient = numpy.polynomial.chebyshev.cheb2poly
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
             elif self.__nonlinear_function is nlsp.nonlinear_function.Hermite:
                 polynomial = numpy.polynomial.hermite_e.HermiteE
                 coefficient = numpy.polynomial.hermite_e.herme2poly
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
             elif self.__nonlinear_function is nlsp.nonlinear_function.Legendre:
                 polynomial = numpy.polynomial.hermite_e.HermiteE
                 coefficient = numpy.polynomial.hermite_e.herme2poly
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
         else:
-            for i in degree:
+            for i in degree_nl:
                 self._nonlinear_functions.append(self.__nonlinear_function(degree=i))
             if self.__nonlinear_function is nlsp.nonlinear_function.Chebyshev:
                 polynomial = numpy.polynomial.chebyshev.Chebyshev
                 coefficient = numpy.polynomial.chebyshev.cheb2poly
-                temp = anytopower(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
+                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
             elif self.__nonlinear_function is nlsp.nonlinear_function.Hermite:
                 polynomial = numpy.polynomial.hermite_e.HermiteE
                 coefficient = numpy.polynomial.hermite_e.herme2poly
-                temp = anytopower(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
+                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
             elif self.__nonlinear_function is nlsp.nonlinear_function.Legendre:
                 polynomial = numpy.polynomial.legendre.Legendre
                 coefficient = numpy.polynomial.legendre.leg2poly
-                temp = anytopower(degree=degree,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
+                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
 
 def powertoany(degree,ip_filter_kernels,polynomial,coefficient):
     ip_filter_kernels_spec = []
@@ -92,7 +92,6 @@ def powertoany(degree,ip_filter_kernels,polynomial,coefficient):
         coefficients.append(coeff)
     coefficients = numpy.asarray(coefficients)
     coefficients = numpy.transpose(coefficients)
-    print coefficients
     coefficients = numpy.linalg.inv(coefficients)
     coefficients = numpy.delete(coefficients, numpy.s_[::len(degree)], 1)
     constants = numpy.transpose(coefficients[0])
@@ -116,24 +115,24 @@ def powertoany(degree,ip_filter_kernels,polynomial,coefficient):
     return op_filter_kernels_s
 
 
-def anytopower(degree,ip_filter_kernels,polynomial,coefficient):
+def anytopower(degree=None,ip_filter_kernels=None,polynomial=None,coefficient=None):
     ip_filter_kernels_spec = []
     for kernel in ip_filter_kernels:
         ip_filter_kernels_spec.append(sumpf.modules.FourierTransform(kernel).GetSpectrum())
-    degree.insert(0,0)
+    degree_temp = numpy.insert(degree,0,0)
     op_filter_kernels_s = []
     coefficients = []
-    for d in degree:
+    for d in degree_temp:
         temp = [0,]*(d)
         temp.append(1)
         poly = polynomial(temp)
         coef = coefficient(poly.coef)
-        coeff = numpy.append(coef,[0,]*(max(degree)+1-len(coef)))
+        coeff = numpy.append(coef,[0,]*(max(degree_temp)+1-len(coef)))
         coefficients.append(coeff)
     coefficients = numpy.asarray(coefficients)
     coefficients = numpy.transpose(coefficients)
     # coefficients = numpy.linalg.inv(coefficients)
-    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree)], 1)
+    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree_temp)], 1)
     constants = numpy.transpose(coefficients[0])
     coefficients = coefficients[1:]
     constant_specs = []
--- tests/test_model_generator/test_nonlinear_function.py
+++ tests/test_model_generator/test_nonlinear_function.py
@@ -1,8 +1,10 @@
+import itertools
 import sumpf
 import nlsp
 
 def test_recomputefilterkernels():
-    ref_nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=[nlsp.nonlinear_function.Power(degree=i+1) for i in range(5)],
+    branches = 3
+    ref_nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=[nlsp.nonlinear_function.Power(degree=i+1) for i in range(branches)],
                                               aliasing_compensation=nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation())
     system_iden = nlsp.system_identification.SineSweep()
     excitation = system_iden.GetExcitation()
@@ -11,9 +13,9 @@ def test_recomputefilterkernels():
     system_iden.SetResponse(response)
     output_model = system_iden.GetOutputModel()
     recompute_filterkernels = nlsp.RecomputeFilterKernels(input_model=output_model)
-    recompute_filterkernels.SetNonlinearFunction(nonlinearfunction=nlsp.nonlinear_function.Hermite)
+    recompute_filterkernels.SetNonlinearFunction(nonlinearfunction=nlsp.nonlinear_function.Chebyshev)
     modified_output_model = recompute_filterkernels.GetOutputModel()
-    sample_signal = sumpf.modules.SweepGenerator().GetSignal()
+    sample_signal = sumpf.modules.NoiseGenerator().GetSignal()
     output_model.SetInput(sample_signal)
     modified_output_model.SetInput(sample_signal)
     ref_nlsystem.SetInput(sample_signal)
@@ -26,22 +28,28 @@ def test_recomputefilterkernels():
     nlsp.plots.relabelandplot(modified_output_model.GetOutput(),label="identified_m",show=True)
 
 def test_filter_kernels():
+    nl_func = [nlsp.nonlinear_function.Power, nlsp.nonlinear_function.Chebyshev, nlsp.nonlinear_function.Legendre,
+               nlsp.nonlinear_function.Hermite]
     branches = 3
-    ref_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches,filter_length=2**10)
-    ref_nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=[nlsp.nonlinear_function.Power(degree=i+1) for i in range(branches)],
-                                              aliasing_compensation=nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation(),
-                                              filter_impulseresponses=ref_filters)
-    ref_nlsystem1= ref_nlsystem
-    recompute_filterkernels = nlsp.RecomputeFilterKernels(input_model=ref_nlsystem1)
-    recompute_filterkernels.SetNonlinearFunction(nonlinearfunction=nlsp.nonlinear_function.Hermite)
-    found_filter_kernels = recompute_filterkernels._filter_impulseresponses
-    mod_system = nlsp.HammersteinGroupModel(nonlinear_functions=[nlsp.nonlinear_function.Hermite(degree=i+1) for i in range(branches)],
-                                              aliasing_compensation=nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation(),
-                                              filter_impulseresponses=found_filter_kernels)
-    ip = sumpf.modules.NoiseGenerator().GetSignal()
-    ref_nlsystem.SetInput(ip)
-    mod_system.SetInput(ip)
-    evaluation = nlsp.evaluations.CompareWithReference(reference_signal=ref_nlsystem.GetOutput(), signal_to_be_evaluated=mod_system.GetOutput())
-    print evaluation.GetSignaltoErrorRatio()
+    ref_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches,filter_length=2**15)
+    for nl1,nl2 in itertools.combinations(nl_func,2):
+        ref_nlsystem = nlsp.HammersteinGroupModel(nonlinear_functions=[nl1(degree=i+1) for i in range(branches)],
+                                                  aliasing_compensation=nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation(),
+                                                  filter_impulseresponses=ref_filters)
+        ref_nlsystem1= ref_nlsystem
+        recompute_filterkernels = nlsp.RecomputeFilterKernels(input_model=ref_nlsystem1)
+        recompute_filterkernels.SetNonlinearFunction(nonlinearfunction=nl2)
+        found_filter_kernels = recompute_filterkernels._filter_impulseresponses
+        mod_system = nlsp.HammersteinGroupModel(nonlinear_functions=[nl2(degree=i+1) for i in range(branches)],
+                                                  aliasing_compensation=nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation(),
+                                                  filter_impulseresponses=found_filter_kernels)
+        ip = sumpf.modules.NoiseGenerator().GetSignal()
+        ref_nlsystem.SetInput(ip)
+        mod_system.SetInput(ip)
+        evaluation = nlsp.evaluations.CompareWithReference(reference_signal=ref_nlsystem.GetOutput(), signal_to_be_evaluated=mod_system.GetOutput())
+        ser = evaluation.GetSignaltoErrorRatio()[0]
+        print ser, nl1, nl2
+        assert ser > 300
 
-test_filter_kernels()
+
+test_recomputefilterkernels()
