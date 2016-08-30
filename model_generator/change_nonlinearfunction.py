5c56a4d44df982bc7e663f57213cf95f58838330
Logesh <logeshthamil@gmail.com>
2016-08-30 11:36:52 +0200

    Recompute filter kernels working fine,

--- model_generator/change_nonlinearfunction.py
+++ model_generator/change_nonlinearfunction.py
@@ -3,14 +3,19 @@ import sumpf
 import nlsp
 from nlsp.nonlinear_functions.nonlinear_functions import PolynomialNonlinearBlock
 import numpy
+from .recompute_filterkernels import anytopower, powertoany
 
 class RecomputeFilterKernels(HGMModelGenerator):
     """
     A class to recompute the filter kernels based on the nonlinear function of the HGM.
     """
-    def __init__(self, input_model=None, nonlinear_function=None, filter_impulseresponses=None):
-        HGMModelGenerator.__init__(self, input_model=input_model, filter_impulseresponses=filter_impulseresponses)
-        self.__nonlinear_function = nonlinear_function
+
+    def __init__(self, input_model=None, nonlinear_function=None):
+        HGMModelGenerator.__init__(self, input_model=input_model)
+        if nonlinear_function is None:
+            self.__desired_nonlinear_function = nlsp.nonlinear_function.Power
+        else:
+            self.__desired_nonlinear_function = nonlinear_function
         self._FindFilterKernels()
 
     @sumpf.Input(PolynomialNonlinearBlock, "GetOutputModel")
@@ -19,136 +24,71 @@ class RecomputeFilterKernels(HGMModelGenerator):
         Set the nonlinear function of the HGM.
         @param nonlinearfunction: the nonlinear function Eg, nlsp.nonlinear_function.Power
         """
-        self.__nonlinear_function = nonlinearfunction
+        self.__desired_nonlinear_function = nonlinearfunction
         self._FindFilterKernels()
 
     def _FindFilterKernels(self):
         """
         Recompute the filter kernels.
         """
+        # get the parameters of the model
         modify_model = nlsp.ModifyModel(input_model=self._input_model)
-        ref_nl = modify_model._nonlinear_functions
-        ref_nl_specific = ref_nl[0].__class__
-        ref_filter = modify_model._filter_impulseresponses
-        degree_nl = []
+        reference_nlfunctions = modify_model._nonlinear_functions
+        reference_nlclass = reference_nlfunctions[0].__class__
+        reference_filterkernels = modify_model._filter_impulseresponses
+        reference_degreeofnl = []
         self._nonlinear_functions = []
-        for nl in ref_nl:
-            degree_nl.append(nl.GetMaximumHarmonics())
-        if self.__nonlinear_function is None:
-            for nl in ref_nl:
-                self._nonlinear_functions.append(nl.CreateModified())
-            self._filter_impulseresponses = ref_filter
-        elif self.__nonlinear_function is ref_nl_specific:
-            for nl in ref_nl:
-                self._nonlinear_functions.append(nl.CreateModified())
-            self._filter_impulseresponses = ref_filter
-        elif self.__nonlinear_function is nlsp.nonlinear_function.Power:
-            for i in degree_nl:
-                self._nonlinear_functions.append(self.__nonlinear_function(degree=i))
-            if self.__nonlinear_function is nlsp.nonlinear_function.Chebyshev:
-                polynomial = numpy.polynomial.chebyshev.Chebyshev
-                coefficient = numpy.polynomial.chebyshev.cheb2poly
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-            elif self.__nonlinear_function is nlsp.nonlinear_function.Hermite:
-                polynomial = numpy.polynomial.hermite_e.HermiteE
-                coefficient = numpy.polynomial.hermite_e.herme2poly
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-            elif self.__nonlinear_function is nlsp.nonlinear_function.Legendre:
-                polynomial = numpy.polynomial.hermite_e.HermiteE
-                coefficient = numpy.polynomial.hermite_e.herme2poly
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
+        for nl in reference_nlfunctions:
+            reference_degreeofnl.append(nl.GetMaximumHarmonics())
+
+        # if the desired nonlinear function is same as the reference
+        if self.__desired_nonlinear_function is reference_nlclass:
+            self._nonlinear_functions = reference_nlfunctions
+            self._filter_impulseresponses = reference_filterkernels
+
+        # else, the desired nonlinear function is different than the reference
         else:
-            for i in degree_nl:
-                self._nonlinear_functions.append(self.__nonlinear_function(degree=i))
-            if self.__nonlinear_function is nlsp.nonlinear_function.Chebyshev:
-                polynomial = numpy.polynomial.chebyshev.Chebyshev
-                coefficient = numpy.polynomial.chebyshev.cheb2poly
-                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
-            elif self.__nonlinear_function is nlsp.nonlinear_function.Hermite:
-                polynomial = numpy.polynomial.hermite_e.HermiteE
-                coefficient = numpy.polynomial.hermite_e.herme2poly
-                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
-            elif self.__nonlinear_function is nlsp.nonlinear_function.Legendre:
-                polynomial = numpy.polynomial.legendre.Legendre
-                coefficient = numpy.polynomial.legendre.leg2poly
-                temp = anytopower(degree=degree_nl,ip_filter_kernels=ref_filter,polynomial=polynomial,coefficient=coefficient)
-                self._filter_impulseresponses = powertoany(degree=degree_nl,ip_filter_kernels=temp,polynomial=polynomial,coefficient=coefficient)
+            for i in reference_degreeofnl:
+                self._nonlinear_functions.append(self.__desired_nonlinear_function(degree=i))
 
-def powertoany(degree,ip_filter_kernels,polynomial,coefficient):
-    ip_filter_kernels_spec = []
-    for kernel in ip_filter_kernels:
-        ip_filter_kernels_spec.append(sumpf.modules.FourierTransform(kernel).GetSpectrum())
-    degree.insert(0,0)
-    op_filter_kernels_s = []
-    coefficients = []
-    for d in degree:
-        temp = [0,]*(d)
-        temp.append(1)
-        poly = polynomial(temp)
-        coef = coefficient(poly.coef)
-        coeff = numpy.append(coef,[0,]*(max(degree)+1-len(coef)))
-        coefficients.append(coeff)
-    coefficients = numpy.asarray(coefficients)
-    coefficients = numpy.transpose(coefficients)
-    coefficients = numpy.linalg.inv(coefficients)
-    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree)], 1)
-    constants = numpy.transpose(coefficients[0])
-    coefficients = coefficients[1:]
-    constant_specs = []
-    for i,c in enumerate(constants):
-        constant_signal = sumpf.modules.ImpulseGenerator(samplingrate=ip_filter_kernels[0].GetSamplingRate(), length=len(ip_filter_kernels[0])).GetSignal()*c
-        constant_spec = sumpf.modules.FourierTransform(constant_signal).GetSpectrum()
-        constant_spec = constant_spec * ip_filter_kernels_spec[i]
-        constant_specs.append(constant_spec)
-    ip_sub = []
-    for spec, cons in zip(ip_filter_kernels_spec,constant_specs):
-        sub = sumpf.modules.Subtract(value1=spec, value2=cons).GetResult()
-        ip_sub.append(sub)
-    for coeff in coefficients:
-        product = []
-        for i in range(len(coeff)):
-            product.append(ip_sub[i]*coeff[i])
-        filter_kernel = sum(product)
-        op_filter_kernels_s.append(sumpf.modules.InverseFourierTransform(filter_kernel).GetSignal())
-    return op_filter_kernels_s
+            # if the desired nonlinear function is powerseries expansion
+            if self.__desired_nonlinear_function is nlsp.nonlinear_function.Power:
+                polynomial, coefficient = return_polynomial_and_coefficient(nl_func=reference_nlclass)
+                self._filter_impulseresponses = anytopower(degree=reference_degreeofnl,
+                                                           ip_filter_kernels=reference_filterkernels,
+                                                           polynomial=polynomial, coefficient=coefficient)
+
+            # else, the desired nonlinear function is other than powerseries expansion
+            else:
+                # if the reference nonlinear function is powerseries expansion
+                if reference_nlclass is nlsp.nonlinear_function.Power:
+                    polynomial, coefficient = return_polynomial_and_coefficient(
+                        nl_func=self.__desired_nonlinear_function)
+                    self._filter_impulseresponses = powertoany(degree=reference_degreeofnl,
+                                                               ip_filter_kernels=reference_filterkernels,
+                                                               polynomial=polynomial, coefficient=coefficient)
+                # else, the reference nonlinear function is not a powerseries expansion
+                else:
+                    polynomial, coefficient = return_polynomial_and_coefficient(nl_func=reference_nlclass)
+                    temp = anytopower(degree=reference_degreeofnl, ip_filter_kernels=reference_filterkernels,
+                                      polynomial=polynomial, coefficient=coefficient)
+                    polynomial, coefficient = return_polynomial_and_coefficient(
+                        nl_func=self.__desired_nonlinear_function)
+                    self._filter_impulseresponses = powertoany(degree=reference_degreeofnl, ip_filter_kernels=temp,
+                                                               polynomial=polynomial, coefficient=coefficient)
 
 
-def anytopower(degree=None,ip_filter_kernels=None,polynomial=None,coefficient=None):
-    ip_filter_kernels_spec = []
-    for kernel in ip_filter_kernels:
-        ip_filter_kernels_spec.append(sumpf.modules.FourierTransform(kernel).GetSpectrum())
-    degree_temp = numpy.insert(degree,0,0)
-    op_filter_kernels_s = []
-    coefficients = []
-    for d in degree_temp:
-        temp = [0,]*(d)
-        temp.append(1)
-        poly = polynomial(temp)
-        coef = coefficient(poly.coef)
-        coeff = numpy.append(coef,[0,]*(max(degree_temp)+1-len(coef)))
-        coefficients.append(coeff)
-    coefficients = numpy.asarray(coefficients)
-    coefficients = numpy.transpose(coefficients)
-    # coefficients = numpy.linalg.inv(coefficients)
-    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree_temp)], 1)
-    constants = numpy.transpose(coefficients[0])
-    coefficients = coefficients[1:]
-    constant_specs = []
-    for i,c in enumerate(constants):
-        constant_signal = sumpf.modules.ImpulseGenerator(samplingrate=ip_filter_kernels[0].GetSamplingRate(), length=len(ip_filter_kernels[0])).GetSignal()*c
-        constant_spec = sumpf.modules.FourierTransform(constant_signal).GetSpectrum()
-        constant_spec = constant_spec * ip_filter_kernels_spec[i]
-        constant_specs.append(constant_spec)
-    for coeff in coefficients:
-        product = []
-        for i in range(len(coeff)):
-            product.append(ip_filter_kernels_spec[i]*coeff[i])
-        filter_kernel = sum(product)
-        op_filter_kernels_s.append(filter_kernel)
-    op_filters = []
-    for spec,cons in zip(op_filter_kernels_s,constant_specs):
-        op = sumpf.modules.Add(value1=spec,value2=cons).GetResult()
-        op_filters.append(sumpf.modules.InverseFourierTransform(op).GetSignal())
-    return op_filters
+def return_polynomial_and_coefficient(nl_func):
+    if nl_func is nlsp.nonlinear_function.Chebyshev:
+        polynomial = numpy.polynomial.chebyshev.Chebyshev
+        coefficient = numpy.polynomial.chebyshev.cheb2poly
+    elif nl_func is nlsp.nonlinear_function.Hermite:
+        polynomial = numpy.polynomial.hermite_e.HermiteE
+        coefficient = numpy.polynomial.hermite_e.herme2poly
+    elif nl_func is nlsp.nonlinear_function.Legendre:
+        polynomial = numpy.polynomial.legendre.Legendre
+        coefficient = numpy.polynomial.legendre.leg2poly
+    elif nl_func is nlsp.nonlinear_function.Laguerre:
+        polynomial = numpy.polynomial.laguerre.Laguerre
+        coefficient = numpy.polynomial.laguerre.lag2poly
+    return polynomial, coefficient
---
+++ model_generator/recompute_filterkernels.py
@@ -0,0 +1,96 @@
+import numpy
+import sumpf
+
+
+def powertoany(degree, ip_filter_kernels, polynomial, coefficient):
+    """
+    Convert the filter coefficients from the powerseries expansion nonlinear function to any.
+    @param degree: the degree of the nonlinear functions
+    @param ip_filter_kernels: the reference filter kernels
+    @param polynomial: the desired polynomial function
+    @param coefficient: the desired polynomial coefficients
+    """
+    ip_filter_kernels_spec = []
+    for kernel in ip_filter_kernels:
+        ip_filter_kernels_spec.append(sumpf.modules.FourierTransform(kernel).GetSpectrum())
+    degree.insert(0, 0)
+    op_filter_kernels_s = []
+    coefficients = []
+    for d in degree:
+        temp = [0, ] * (d)
+        temp.append(1)
+        poly = polynomial(temp)
+        coef = coefficient(poly.coef)
+        coeff = numpy.append(coef, [0, ] * (max(degree) + 1 - len(coef)))
+        coefficients.append(coeff)
+    coefficients = numpy.asarray(coefficients)
+    coefficients = numpy.transpose(coefficients)
+    coefficients = numpy.linalg.inv(coefficients)
+    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree)], 1)
+    constants = numpy.transpose(coefficients[0])
+    coefficients = coefficients[1:]
+    constant_specs = []
+    for i, c in enumerate(constants):
+        constant_signal = sumpf.modules.ImpulseGenerator(samplingrate=ip_filter_kernels[0].GetSamplingRate(),
+                                                         length=len(ip_filter_kernels[0])).GetSignal() * c
+        constant_spec = sumpf.modules.FourierTransform(constant_signal).GetSpectrum()
+        constant_spec = constant_spec * ip_filter_kernels_spec[i]
+        constant_specs.append(constant_spec)
+    ip_sub = []
+    for spec, cons in zip(ip_filter_kernels_spec, constant_specs):
+        sub = sumpf.modules.Subtract(value1=spec, value2=cons).GetResult()
+        ip_sub.append(sub)
+    for coeff in coefficients:
+        product = []
+        for i in range(len(coeff)):
+            product.append(ip_sub[i] * coeff[i])
+        filter_kernel = sum(product)
+        op_filter_kernels_s.append(sumpf.modules.InverseFourierTransform(filter_kernel).GetSignal())
+    return op_filter_kernels_s
+
+
+def anytopower(degree=None, ip_filter_kernels=None, polynomial=None, coefficient=None):
+    """
+    Convert the filter coefficients from any nonlinear function other than powerseries expansion.
+    @param degree: the degree of the nonlinear functions
+    @param ip_filter_kernels: the reference filter kernels
+    @param polynomial: the desired polynomial function
+    @param coefficient: the desired polynomial coefficients
+    """
+    ip_filter_kernels_spec = []
+    for kernel in ip_filter_kernels:
+        ip_filter_kernels_spec.append(sumpf.modules.FourierTransform(kernel).GetSpectrum())
+    degree_temp = numpy.insert(degree, 0, 0)
+    op_filter_kernels_s = []
+    coefficients = []
+    for d in degree_temp:
+        temp = [0, ] * (d)
+        temp.append(1)
+        poly = polynomial(temp)
+        coef = coefficient(poly.coef)
+        coeff = numpy.append(coef, [0, ] * (max(degree_temp) + 1 - len(coef)))
+        coefficients.append(coeff)
+    coefficients = numpy.asarray(coefficients)
+    coefficients = numpy.transpose(coefficients)
+    # coefficients = numpy.linalg.inv(coefficients)
+    coefficients = numpy.delete(coefficients, numpy.s_[::len(degree_temp)], 1)
+    constants = numpy.transpose(coefficients[0])
+    coefficients = coefficients[1:]
+    constant_specs = []
+    for i, c in enumerate(constants):
+        constant_signal = sumpf.modules.ImpulseGenerator(samplingrate=ip_filter_kernels[0].GetSamplingRate(),
+                                                         length=len(ip_filter_kernels[0])).GetSignal() * c
+        constant_spec = sumpf.modules.FourierTransform(constant_signal).GetSpectrum()
+        constant_spec = constant_spec * ip_filter_kernels_spec[i]
+        constant_specs.append(constant_spec)
+    for coeff in coefficients:
+        product = []
+        for i in range(len(coeff)):
+            product.append(ip_filter_kernels_spec[i] * coeff[i])
+        filter_kernel = sum(product)
+        op_filter_kernels_s.append(filter_kernel)
+    op_filters = []
+    for spec, cons in zip(op_filter_kernels_s, constant_specs):
+        op = sumpf.modules.Add(value1=spec, value2=cons).GetResult()
+        op_filters.append(sumpf.modules.InverseFourierTransform(op).GetSignal())
+    return op_filters
