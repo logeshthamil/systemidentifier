5db5b8d6d75589cf8c19c00b46aeb61d5e153fb2
Logesh <logeshthamil@gmail.com>
2016-08-31 11:42:43 +0200

    test functions for the Save and Retrieve model class written

--- tests/test_model_generator/test_saveandretrievemodel.py
+++ tests/test_model_generator/test_saveandretrievemodel.py
@@ -1,10 +1,10 @@
+import os
 import sumpf
 import nlsp
 
-
 def test_saveandretrieve_accuracy():
-    branches = 10
-    location = "C:\Users\diplomand.8\Desktop\save\some"
+    branches = 5
+    location = "C:\Users\diplomand.8\Desktop\some.npz"
     ref_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches)
     ref_nl = [nlsp.nonlinear_function.Power(degree=i + 1) for i in range(branches)]
     ref_aliasing = nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation()
@@ -19,3 +19,56 @@ def test_saveandretrieve_accuracy():
     evaluation = nlsp.evaluations.CompareWithReference(reference_signal=model.GetOutput(),
                                                        signal_to_be_evaluated=model_retrieved.GetOutput())
     assert evaluation.GetSignaltoErrorRatio() > 500
+    os.remove(location)
+
+
+def test_methods_savemodel():
+    """
+    Test the methods of the SaveHGMModel class.
+    """
+    branches = 5
+    location1 = "C:\Users\diplomand.8\Desktop\some1.npz"
+    location2 = "C:\Users\diplomand.8\Desktop\some2.npz"
+    ref_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches)
+    ref_nl = [nlsp.nonlinear_function.Power(degree=i + 1) for i in range(branches)]
+    ref_aliasing = nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation()
+    model1 = nlsp.HammersteinGroupModel()
+    model2 = nlsp.HammersteinGroupModel(nonlinear_functions=ref_nl, filter_impulseresponses=ref_filters,
+                                        aliasing_compensation=ref_aliasing)
+    save_model = nlsp.SaveHGMModel(filename=location1, model=model1)
+    save_model.SetFilename(location2)
+    save_model.SetModel(model=model1)
+    model1_retrieved = nlsp.RetrieveHGMModel(filename=location1).GetModel()
+    model2_retrieved = nlsp.RetrieveHGMModel(filename=location2).GetModel()
+    sample = sumpf.modules.NoiseGenerator().GetSignal()
+    model1.SetInput(sample)
+    model1_retrieved.SetInput(sample)
+    evaluation = nlsp.evaluations.CompareWithReference(reference_signal=model1.GetOutput(),
+                                                       signal_to_be_evaluated=model1_retrieved.GetOutput())
+    assert evaluation.GetSignaltoErrorRatio() > 500
+    evaluation.SetReferenceOutput(model2.GetOutput())
+    evaluation.SetIdentifiedOutput(model2_retrieved.GetOutput())
+    assert evaluation.GetSignaltoErrorRatio() > 500
+    os.remove(location1)
+    os.remove(location2)
+
+
+def test_methods_retrievemodel():
+    """
+    Test the methods of the Retrieve model class.
+    """
+    branches = 5
+    location = "C:\Users\diplomand.8\Desktop\some1.npz"
+    ref_filters = nlsp.helper_functions.create_arrayof_bpfilter(branches=branches)
+    ref_nl = [nlsp.nonlinear_function.Power(degree=i + 1) for i in range(branches)]
+    ref_aliasing = nlsp.aliasing_compensation.ReducedUpsamplingAliasingCompensation()
+    model = nlsp.HammersteinGroupModel(nonlinear_functions=ref_nl, filter_impulseresponses=ref_filters,
+                                       aliasing_compensation=ref_aliasing)
+    save_model = nlsp.SaveHGMModel(filename=location, model=model)
+    retrieve_model = nlsp.RetrieveHGMModel()
+    try:
+        retrieve_model.GetModel()
+    except Exception as e:
+        assert str(e) == "Please enter the filename"
+    retrieve_model.SetFilename(location)
+    retrieve_model.GetModel()
